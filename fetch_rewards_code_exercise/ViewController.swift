4ae7219d5ec598b0532a74d97936ada4d8508aba
cam330 <wilcoxcam@gmail.com>
2021-05-24 08:16:04 -0700

    Initial Commit

---
+++ fetch_rewards_code_exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fetch_rewards_code_exercise
+//
+//  Created by Cameron Wilcox on 5/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
